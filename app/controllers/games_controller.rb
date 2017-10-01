11d8be382ac74d42f1f13c8b89b0d951fbcfe431
jack <goodatsports@gmail.com>
2017-10-01 16:11:50 -0700

    Added show/index views for games and users controllers

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,9 @@
 class GamesController < ApplicationController
   def index
     @games = Game.all
-    render :index
   end
 
   def show
     @game = Game.find_by(api_id: params[:id])
-    render :show
   end
 end
